16d12b4fc7eb8efcacd18a55f621fb50e70792f4
visionedge <visionedgeruntime@gmail.com>
2024-08-19 16:52:29 +0530

    ws://localhost:8080

--- js/app.js
+++ js/app.js
@@ -59,8 +59,8 @@
 //   };
 // });
 
-//const connection = new WebSocket("wss://localhost:8080");
-const connection = new WebSocket("wss://chromeos-pwa.onrender.com:8080");
+const connection = new WebSocket("ws://localhost:8080");
+//const connection = new WebSocket("wss://chromeos-pwa.onrender.com:8080");
 const button = document.querySelector("#send");
 
 connection.onopen = (event) => {
