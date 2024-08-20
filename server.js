d7d4ce6c2a1708998c768ec01d6631dcb6619d11
visionedge <visionedgeruntime@gmail.com>
2024-08-20 16:19:02 +0530

    added public

--- server.js
+++ server.js
@@ -89,7 +89,11 @@ const express = require('express');
 const socketIO = require('socket.io');
 const PORT = process.env.PORT || 3000;
 const INDEX = '/index.html';
+// const server = express()
+//   .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+//   .listen(PORT, () => console.log(`Listening on ${PORT}`));
 const server = express()
+  .use(express.static('public')) // relative path of client-side code
   .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
 const io = require("socket.io")(server,{
